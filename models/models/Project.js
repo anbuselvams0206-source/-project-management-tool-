b36277825d695ec4af47e75214b2ede6c9f6178b
anbuselvams0206-source <anbuselvams0206@gmail.com>
2026-06-26 12:24:40 +0530

    Add Project model
    Created the Project model with project name, description, and team members.

---
+++ models/models/Project.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const ProjectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    members: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }]
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Project", ProjectSchema);
